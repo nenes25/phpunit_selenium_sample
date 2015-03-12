e4628da650fdac95c38501cc840507ddf24b57ca
Hervé <nenes_fr@yahoo.fr>
2015-03-12 16:31:56 +0100

    Ajout des fichiers de base

---
+++ SeleniumTest.php
@@ -0,0 +1,32 @@
+<?php
+class SeleniumTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+	$this->sampleUrl = 'http://www.google.fr';
+
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl($this->sampleUrl);
+    }
+
+	/**
+	 * Résultat OK
+	 */
+    public function testTitleOk()
+    {
+        $this->url($this->sampleUrl);
+        $this->assertEquals('Google', $this->title());
+    }
+	
+	/**
+	 * Resultat Ko
+	 */
+	public function testTitleKo()
+    {
+        $this->url($this->sampleUrl);
+        $this->assertEquals('Wrong Title', $this->title());
+    }
+
+}
+?>
