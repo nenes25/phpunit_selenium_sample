391a8173341299d6d063a7f389c9a9e9ce4ff0de
Hervé <nenes_fr@yahoo.fr>
2015-03-17 13:00:23 +0100

    - correction include

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,5 @@
  * PhpUnit Sample data
  */
 
-include_once dirname(__FILE__).'BasicTest.php';
-include_once dirname(__FILE__).'SeleniumTest.php';
+include_once dirname(__FILE__).'/BasicTest.php';
+include_once dirname(__FILE__).'/SeleniumTest.php';
